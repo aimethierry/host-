26463b914c493508940aca662be92ddaf1aaaa48
aimethierry <aime.thierry97@gmail.com>
2017-11-03 14:16:09 -0700

    wsgi changed

--- Scripts/client/client/wsgi.py
+++ Scripts/client/client/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "client.settings")
 
 application = get_wsgi_application()

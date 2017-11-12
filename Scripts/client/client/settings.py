7d2094282e290be2db82af4f539c51d753f6ccd8
aimethierry <aime.thierry97@gmail.com>
2017-11-11 16:15:27 -0800

    second change

--- Scripts/client/client/settings.py
+++ Scripts/client/client/settings.py
@@ -131,3 +131,10 @@ STATICFILES_DIRS = [
 # will be served
 STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static-server") 
 
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
